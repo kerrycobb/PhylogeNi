9d55433e58167e39f8b4c7b2c98755cd169f7e77
Kerry Cobb <cobbkerry@gmail.com>
2022-10-10 23:58:48 -0500

    Comments on tests that should be added

--- tests/test_parseNewick.nim
+++ tests/test_parseNewick.nim
@@ -31,9 +31,18 @@ suite "Parse Valid Trees":
     check(toSeq(t.preorder, length) == "1.0 1.0 1.0")
     check(toSeq(t.preorder, data) == "data data data")
 
+  # TODO: Write tests for these 
+  # echo newTreeFromString(";")
+  # echo newTreeFromString("A;")
+  # echo newTreeFromString("();")
+  # echo newTreeFromString("(B)A;")
+  # echo newTreeFromString("(B,C,D)A;")
+  # echo newTreeFromString("((D,E)C,B)A;")
+  # echo newTreeFromString("((A,B),C);")
 
 
-suite "Parse Invalid Trees":
+
+test "Parse Invalid Trees":
   var t = Tree[void]()
 
   proc testParse(str, expected: string) = 
@@ -42,24 +51,22 @@ suite "Parse Invalid Trees":
       let msg = getCurrentExceptionMsg()
       check(msg == expected)
     
-  test "invalid 1":
-    testParse("&r](B,C)A;", "Unexpected character \"]\" at line 1, column 3")
-  test "invalid 2":
-    testParse("[&x](B,C)A;", "Unexpected character \"x\" at line 1, column 3")
-  test "invalid 3":
-    testParse("[&r(B,C)A;", "Expected \"]\" at line 1, column 4")
-  test "invalid 4":
-    testParse("[&r]((B,C,D)A;", "Mismatched parentheses at line 1, column 14")
-  test "invalid 5":
-    testParse("[&r](B\",C)A;", "Unexpected \" at line 1, column 7")
-  test "invalid 6":
-    testParse("[&r](B B,C)A;", "Unexpected character \"B\" at line 1, column 8")
-  test "invalid 7":
-    testParse("[&r](\"B B\"\"B B\",\"C C\")\"A A\";", "Unexpected \" at line 1, column 11")
-  test "invalid 8":
-    testParse("[&r](B,C)A", "Unexpected end of stream at line 1, column 11")
-  test "invalid 9":
-    testParse("[&r](B:1.0,C:1.0)A:1.0", "Unexpected end of stream at line 1, column 23")
+  testParse("&r](B,C)A;", "Unexpected character \"]\" at line 1, column 3")
+  testParse("[&x](B,C)A;", "Unexpected character \"x\" at line 1, column 3")
+  testParse("[&r(B,C)A;", "Expected \"]\" at line 1, column 4")
+  testParse("[&r]((B,C,D)A;", "Mismatched parentheses at line 1, column 14")
+  testParse("[&r](B\",C)A;", "Unexpected \" at line 1, column 7")
+  testParse("[&r](B B,C)A;", "Unexpected character \"B\" at line 1, column 8")
+  testParse("[&r](\"B B\"\"B B\",\"C C\")\"A A\";", "Unexpected \" at line 1, column 11")
+  testParse("[&r](B,C)A", "Unexpected end of stream at line 1, column 11")
+  testParse("[&r](B:1.0,C:1.0)A:1.0", "Unexpected end of stream at line 1, column 23")
+
+  # TODO: Write tests for these 
+  # discard newTreeFromString("")
+  # discard newTreeFromString("A")
+  # discard newTreeFromString("()")
+  # discard newTreeFromString("(A,B;")
+  # discard newTreeFromString(",;")
     
     
     
